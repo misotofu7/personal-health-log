b91d7da1b74d9bc7ee6e90cf993a94f83bc3bc05
Kaylie Chang <firenachang@gmail.com>
2026-01-16 23:24:28 -0800

    style: add initial homepage layout and global styles
    - Add centered title and text box input
    - Used Inter font
    - Set up (in globals.css spacing and other styling)

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
